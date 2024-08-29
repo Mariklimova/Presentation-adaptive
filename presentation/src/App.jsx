8443a0de263e09370635a9c8938cbd397080f08e
Mariklimova <mariku22@yandex.ru>
2024-08-29 22:33:51 +0300

    added routes

--- presentation/src/App.jsx
+++ presentation/src/App.jsx
@@ -1,10 +1,15 @@
 
-import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/HomePage/Home'
+import SingIn from './pages/SingIn/SingIn'
 
 function App() {
   
   return <>
-      <p>hello</p>
+      <Routes>
+        <Route path = '/' element = {<SingIn/>}></Route>
+        <Route path = '/home' element = {<Home/>}></Route>
+      </Routes>
     </>
 }
 
