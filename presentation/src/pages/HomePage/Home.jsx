470f6a1ba5d3a95e3caf185f80b763a4baac7919
Mariklimova <mariku22@yandex.ru>
2024-08-30 01:25:37 +0300

    added home page

--- presentation/src/pages/HomePage/Home.jsx
+++ presentation/src/pages/HomePage/Home.jsx
@@ -1,7 +1,17 @@
-
+import style from './home.module.css'
 
 export default function Home() {
-    return<>
-    <h1>hello</h1>
+    return <>
+        <main className={style.background}>
+            <div className={style.content}>
+                <div className={style.info}>
+                    <h1>Демо-версия</h1>
+                    <p>За 18 лет деятельности было реализовано более 500 крупных, а так же, не менее 2500 средних и малых проектов. Используя знания и накопленный опыт мы можем предложить </p>
+                    <button className={style.try_btn}>Попробовать бесплатно<image className={style.arrow}></image></button>
+                </div>
+                <div className={style.image}></div>
+            </div>
+
+        </main>
     </>
 }
