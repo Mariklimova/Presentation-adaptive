c0c34b389b130bd6fbc17ff1dbfcd19807cec8c8
Mariklimova <mariku22@yandex.ru>
2024-08-31 01:18:50 +0300

    added home page & adaptive

--- presentation/src/pages/HomePage/Home.jsx
+++ presentation/src/pages/HomePage/Home.jsx
@@ -7,11 +7,13 @@ export default function Home() {
                 <div className={style.info}>
                     <h1>Демо-версия</h1>
                     <p>За 18 лет деятельности было реализовано более 500 крупных, а так же, не менее 2500 средних и малых проектов. Используя знания и накопленный опыт мы можем предложить </p>
-                    <button className={style.try_btn}>Попробовать бесплатно<image className={style.arrow}></image></button>
+                    <button className={style.try_btn}>Попробовать бесплатно<div className={style.arrow}></div></button>
                 </div>
+
                 <div className={style.image}></div>
             </div>
 
-        </main>
+
+        </main >
     </>
 }
