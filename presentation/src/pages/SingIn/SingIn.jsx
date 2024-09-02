5b8f19ed2b0d4c5a3cfad97037032d4700757b1c
Mariklimova <mariku22@yandex.ru>
2024-09-02 22:27:33 +0300

    added singIn page

--- presentation/src/pages/SingIn/SingIn.jsx
+++ presentation/src/pages/SingIn/SingIn.jsx
@@ -1,11 +1,29 @@
-import style from './singin.module.css'
+import style from './singin.module.css';
+
 export default function SingIn() {
     return <>
 
         <div className={style.wrapper}>
-           <button>X</button>
+            <button className={style.close}><image className={style.cross}></image></button>
+
             <h1>Войти в систему</h1>
-        </div>
+            <div className={style.data_entry}>
+                <input type="text" placeholder='Email/Телефон' />
+                <input type="password" placeholder='Пароль' />
+            </div>
+            <label>
+                <input className={style.checkbox} type="checkbox" />
+                Запомнить пароль
+            </label>
+            <h2>Восстановить</h2>
+
+            <div className={style.singin_singup}>
+                <button><a href ={'/home'} >Войти</a></button>
+                <button>Зарегистрироваться</button>
+            </div>
+
+
+        </div >
 
     </>
 }
